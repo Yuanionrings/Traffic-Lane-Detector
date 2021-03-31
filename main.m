38796097923e4140a4a23bb08b1ccbf2f922b39b
Yuan <yuanionrings@gmail.com>
2021-03-31 16:11:03 -0400

    added conversion to grayscale, hsv, and hsl

--- main.m
+++ main.m
@@ -5,9 +5,43 @@
 
 clc; close all; clear;
 
-A = imread('resource\test.jpg');
-imshow(A);
-title('test');
+% Scan in Image
+test = imread('resource\test.jpg');
+rgb = double(test) / 255;
+
+% Convert to Grayscale
+G = rgb2gray(rgb);
+
+% Darken Grayscale
+D = imadjust(G);
+
+% RGB to HSV
+HSV = rgb2hsv(rgb);
+
+% RGB to HSL
+HSL = rgb2hsl(rgb);
+
+% Show Original Image
+subplot(2, 3, 1);
+imshow(rgb);
+title('original');
+
+subplot(2, 3, 2);
+imshow(G);
+title('grayscale');
+
+subplot(2, 3, 3);
+imshow(D);
+title('darkened grayscale');
+
+subplot(2, 3, 4);
+imshow(HSV);
+title('HSV');
+
+subplot(2, 3, 5);
+imshow(HSL);
+title('HSL');
+
 pause;
 
 
