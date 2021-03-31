6df22927c4c05cb441cb5ced28cdf4a20b15c5dc
Yuan <yuanionrings@gmail.com>
2021-03-31 12:51:49 -0400

    added initial matlab script and resource file for testing images/videos

---
+++ main.m
@@ -0,0 +1,14 @@
+% COMP3800 – Image Processing
+% Traffic Lane Detector
+% Yuan Gao, Will Eddy, Tyler
+% 03/31/2021
+
+clc; close all; clear;
+
+A = imread('resource\test.jpg');
+imshow(A);
+title('test');
+pause;
+
+
+clc; close all; clear;
