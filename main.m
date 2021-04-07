a0b375443fed16d1c8164fa900928ee875378e5e
Wil <eddyw@wit.edu>
2021-04-07 16:11:12 -0400

    Finished pipeline for wil's specific images

--- main.m
+++ main.m
@@ -6,7 +6,8 @@
 clc; close all; clear;
 
 % Scan in Image
-test = imread('resource\test1.jpg');
+test = imread('resource\wet1.jpg');
+test = imrotate(test,270);
 rgb = double(test) / 255;
 
 % Convert to Grayscale
@@ -86,6 +87,12 @@ Combined_Image_Gaussian_Blur = imgaussfilt(C,2);
 % Apply Canny Edge Detection
 Edge_Detection_Image = edge(Combined_Image_Gaussian_Blur, 'canny');
 
+% Perform ROI
+% don't know how to handle this, should we make dynamic for pictures
+% or just hard code for our images?
+% these params are xmin yim width height
+Cropped_Image = imcrop(Edge_Detection_Image,[0 1200 3024 1400]);
+
 % Show Combined Image
 subplot(2, 2, 1);
 imshow(C);
@@ -103,13 +110,27 @@ title('Edge_Detection_Image');
 
 % Show Region of Interest
 subplot(2, 2, 4);
-imshow(ROI);
+imshow(Cropped_Image);
 title('Region of Interest')
 
 pause;
 
+% Perform Hough Transform
+[H,T,R] = hough(Cropped_Image);
+P = houghpeaks(H,5,'threshold',ceil(0.3*max(H(:))));
+x = T(P(:,2)); y = R(P(:,1));
+lines = houghlines(Cropped_Image,T,R,P,'FillGap',5,'MinLength',7);
+figure, imshow(G), hold on
+max_len = 0;
+for k = 1:length(lines)
+   xy = [lines(k).point1; lines(k).point2];
+   plot(xy(:,1),xy(:,2)+1200,'LineWidth',2,'Color','green');
 
+   % Plot beginnings and ends of lines
+   %plot(xy(1,1),xy(1,2),'x','LineWidth',2,'Color','yellow');
+   %plot(xy(2,1),xy(2,2),'x','LineWidth',2,'Color','red');
+end
 
-
+pause;
 
 clc; close all; clear;
